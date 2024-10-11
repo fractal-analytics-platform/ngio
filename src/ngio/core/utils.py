4190168daa493dea5cfecf0830d55b92facd5551
lorenzo <lorenzo.cerrone@uzh.ch>
2024-10-11 15:28:19 +0200

    refactor build empty ngff image

--- src/ngio/core/utils.py
+++ src/ngio/core/utils.py
@@ -15,6 +15,62 @@ from ngio.ngff_meta.fractal_image_meta import (
 )
 
 
+def _build_empty_pyramid(
+    group,
+    image_meta,
+    shape: list[int],
+    chunks: list[int] | None = None,
+    dtype: str = "uint16",
+    on_disk_axis: list[str] = ("t", "c", "z", "y", "x"),
+    xy_scaling_factor: float = 2.0,
+    z_scaling_factor: float = 1.0,
+):
+    # Return the an Image object
+    scaling_factor = []
+    for ax in on_disk_axis:
+        if ax in ["x", "y"]:
+            scaling_factor.append(xy_scaling_factor)
+        elif ax == "z":
+            scaling_factor.append(z_scaling_factor)
+        else:
+            scaling_factor.append(1.0)
+
+    for dataset in image_meta.datasets:
+        path = dataset.path
+
+        # V3
+        # group.create_array(
+        #    name=path, fill_value=0, shape=shape, dtype=dtype, chunks=chunks,
+        # )
+
+        group.zeros(
+            name=path,
+            shape=shape,
+            dtype=dtype,
+            chunks=chunks,
+            dimension_separator="/",
+        )
+
+        # Todo redo this with when a proper build of pyramid is implemented
+        _shape = []
+        for s, sc in zip(shape, scaling_factor, strict=True):
+            if math.floor(s / sc) % 2 == 0:
+                _shape.append(math.floor(s / sc))
+            else:
+                _shape.append(math.ceil(s / sc))
+        shape = list(_shape)
+
+        if chunks is not None:
+            _chunks = []
+            for c, sc in zip(chunks, scaling_factor, strict=True):
+                if math.floor(c / sc) % 2 == 0:
+                    _chunks.append(math.floor(c / sc))
+                else:
+                    _chunks.append(math.ceil(c / sc))
+            chunks = list(_chunks)
+    return None
+
+
 def create_empty_ome_zarr_image(
     store: StoreLike,
     shape: list[int],
@@ -77,39 +133,18 @@ def create_empty_ome_zarr_image(
     )
     meta_handler.write_meta(image_meta)
     group = meta_handler.group
+
     # Create the empty image at each level in the pyramid
-
-    # Return the an Image object
-    scaling_factor = []
-    for ax in on_disk_axis:
-        if ax in ["x", "y"]:
-            scaling_factor.append(xy_scaling_factor)
-        elif ax == "z":
-            scaling_factor.append(z_scaling_factor)
-        else:
-            scaling_factor.append(1.0)
-
-    for dataset in image_meta.datasets:
-        path = dataset.path
-
-        # V3
-        # group.create_array(
-        #    name=path, fill_value=0, shape=shape, dtype=dtype, chunks=chunks,
-        # )
-
-        group.zeros(
-            name=path,
-            shape=shape,
-            dtype=dtype,
-            chunks=chunks,
-            dimension_separator="/",
-        )
-
-        # Todo redo this with when a proper build of pyramid is implemented
-        shape = [int(s / sc) for s, sc in zip(shape, scaling_factor, strict=True)]
-
-        if chunks is not None:
-            chunks = [int(c / sc) for c, sc in zip(chunks, scaling_factor, strict=True)]
+    _build_empty_pyramid(
+        group=group,
+        image_meta=image_meta,
+        shape=shape,
+        chunks=chunks,
+        dtype=dtype,
+        on_disk_axis=on_disk_axis,
+        xy_scaling_factor=xy_scaling_factor,
+        z_scaling_factor=z_scaling_factor,
+    )
 
 
 def create_empty_ome_zarr_label(
@@ -155,48 +190,15 @@ def create_empty_ome_zarr_label(
     meta_handler.write_meta(image_meta)
     group = meta_handler.group
     group.attrs["image-label"] = {"version": version, "source": {"image": "../../"}}
+
     # Create the empty image at each level in the pyramid
-
-    # Return the an Image object
-    scaling_factor = []
-    for ax in on_disk_axis:
-        if ax in ["x", "y"]:
-            scaling_factor.append(xy_scaling_factor)
-        elif ax == "z":
-            scaling_factor.append(z_scaling_factor)
-        else:
-            scaling_factor.append(1.0)
-
-    for dataset in image_meta.datasets:
-        path = dataset.path
-
-        # V3
-        # group.create_array(
-        #    name=path, fill_value=0, shape=shape, dtype=dtype, chunks=chunks,
-        # )
-
-        group.zeros(
-            name=path,
-            shape=shape,
-            dtype=dtype,
-            chunks=chunks,
-            dimension_separator="/",
-        )
-
-        # Todo redo this with when a proper build of pyramid is implemented
-        _shape = []
-        for s, sc in zip(shape, scaling_factor, strict=True):
-            if math.floor(s / sc) % 2 == 0:
-                _shape.append(math.floor(s / sc))
-            else:
-                _shape.append(math.ceil(s / sc))
-        shape = list(_shape)
-
-        if chunks is not None:
-            _chunks = []
-            for c, sc in zip(chunks, scaling_factor, strict=True):
-                if math.floor(c / sc) % 2 == 0:
-                    _chunks.append(math.floor(c / sc))
-                else:
-                    _chunks.append(math.ceil(c / sc))
-            chunks = list(_chunks)
+    _build_empty_pyramid(
+        group=group,
+        image_meta=image_meta,
+        shape=shape,
+        chunks=chunks,
+        dtype=dtype,
+        on_disk_axis=on_disk_axis,
+        xy_scaling_factor=xy_scaling_factor,
+        z_scaling_factor=z_scaling_factor,
+    )
