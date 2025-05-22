b6addfc0f6f570fe5142e2a88e0a3d66b6672da7
lorenzo <lorenzo.cerrone@uzh.ch>
2025-05-22 15:06:57 +0200

    add url validation to fractal fsspec store

--- src/ngio/utils/_fractal_fsspec_store.py
+++ src/ngio/utils/_fractal_fsspec_store.py
@@ -1,4 +1,7 @@
 import fsspec.implementations.http
+from aiohttp import ClientResponseError
+
+from ngio.utils import NgioValueError
 
 
 def fractal_fsspec_store(
@@ -9,5 +12,31 @@ def fractal_fsspec_store(
     if fractal_token is not None:
         client_kwargs["headers"] = {"Authorization": f"Bearer {fractal_token}"}
     fs = fsspec.implementations.http.HTTPFileSystem(client_kwargs=client_kwargs)
+
     store = fs.get_mapper(url)
+
+    possible_keys = [".zgroup", ".zarray"]
+    for key in possible_keys:
+        try:
+            value = store.get(key)
+            if value is not None:
+                break
+        except ClientResponseError as e:
+            if e.status == 401 and fractal_token is None:
+                raise NgioValueError(
+                    "No auto token is provided. You need a valid "
+                    f"'fractal_token' to access: {url}."
+                ) from e
+            elif e.status == 401 and fractal_token is not None:
+                raise ValueError(
+                    f"The 'fractal_token' provided is invalid for: {url}."
+                ) from e
+            else:
+                raise e
+    else:
+        raise NgioValueError(
+            f"Store {url} can not be read. Possible problems are: \n"
+            "- The url does not exist. \n"
+            f"- The url is not a valid .zarr. \n"
+        )
     return store
